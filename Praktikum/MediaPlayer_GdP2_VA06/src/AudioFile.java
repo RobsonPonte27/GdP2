8da2a0eaed89f9567f6b98bfc83359415bbfeb7b
Lars Tragl <lars@vmmint13-ss14.(none)>
2014-04-10 13:46:33 +0200

    Bearbeitete 1a

--- Praktikum/MediaPlayer_GdP2_VA06/src/AudioFile.java
+++ Praktikum/MediaPlayer_GdP2_VA06/src/AudioFile.java
@@ -21,11 +21,32 @@ public class AudioFile {
 		return pathname;
 	}
 	
+	
 	public String getFilename () {
 		return filename;
 	}
 	
+	
+	// public methods
 	public void parsePathname (String pathname) {
+		
+		// slashes richten
+		pathname = pathname.replace('\\', java.io.File.separatorChar);
+		pathname = pathname.replace('/', java.io.File.separatorChar);
+		
+		// doppelte slashes sollen rausfallen
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0, ib = 0; i < pathname.length(); i++) {
+			if (pathname.charAt(i) != java.io.File.separatorChar || ib == 0 || buffer.charAt(ib-1) != java.io.File.separatorChar) {
+				buffer.append(pathname.charAt(i));
+				ib++;
+			}
+		}
+		
+		// convertiere buffer
+		pathname = buffer.toString();
+		
+		System.out.println(pathname);
 		
 	}
 	
